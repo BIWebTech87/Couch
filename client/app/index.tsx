65ff87831e941d7a452aa21c389e1e64db311e16
BIWebtech87 <boris.isac@webtech87.pt>
2025-05-21 23:22:25 +0100

    [ADD]  react native

---
+++ client/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        borderWidth: 2,
+        margin: 'auto',
+        width: '90%',
+      }}
+    >
+      <Text>Boris {'\n'} Isac!!!</Text>
+    </View>
+  );
+}
